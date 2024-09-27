2bd70eacb0a03437c67e47e3d7175f3b1bcb9c6a
ramya <ramyapriyasankar@gmail.com>
2024-09-27 08:39:02 +0530

    Kitchen Timings

---
+++ Kitchen Timings.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+	int t, x, y, tot;
+	scanf("%d", &t);
+	while (t--)
+	{
+	    scanf("%d %d", &x, &y);
+	    tot = y-x;
+	    printf("%d\n", tot);
+	    
+	}
+	    return 0;                      
+
+}
+
