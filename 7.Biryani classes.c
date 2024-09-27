681ee925048b5f33734cec02db99af3e1d1eea61
ramya <ramyapriyasankar@gmail.com>
2024-09-27 08:33:46 +0530

    7.Biryani classes

---
+++ 7.Biryani classes.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+	int t, x, y, total;
+	scanf("%d", &t);
+	while (t--)
+	{
+	    scanf("%d %d", &x, &y);
+	    total = x*y;
+	    printf("%d\n", total);
+	    
+	}
+	return 0;
+
+}
+
