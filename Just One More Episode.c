899ba90fa4e29de6ae68119884a8f6e8129d4b01
ramya <ramyapriyasankar@gmail.com>
2024-09-28 04:39:18 +0530

    Just One More Episode

---
+++ Just One More Episode.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+	int t, x;
+	scanf("%d", &t);
+	while (t--)
+	{
+	    scanf("%d", &x);
+	   if (x>24) 
+	   {
+	       printf("YES\n");
+	   }
+	   else 
+	   {
+	       printf("NO\n");
+	   }
+	}
+	return 0;
+
+}
