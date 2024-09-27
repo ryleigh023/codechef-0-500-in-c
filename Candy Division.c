a7ca671cb40c501df34321eb3efe4c00bcea882d
ramya <ramyapriyasankar@gmail.com>
2024-09-27 09:17:17 +0530

    Candy Division

---
+++ Candy Division.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+	int t, n;
+	scanf("%d", &t);
+	while (t--)
+	{
+	    scanf("%d", &n);
+	    if (n%3==0)
+	    {
+	        printf("YES\n");
+	    } 
+	    else 
+	    { 
+	        printf("NO\n");
+    	}
+	}
+	return 0;
+
+}
+
