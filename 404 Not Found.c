c84c00f2e7fd0085eb655c132a11c28b43414942
ramya <ramyapriyasankar@gmail.com>
2024-09-27 08:37:25 +0530

    404 Not Found

---
+++ 404 Not Found.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+            int x;
+            scanf("%d", &x);
+            if (x == 404)
+            {
+                printf("NOT FOUND");
+                
+            }
+            else
+            {
+                printf("FOUND");
+            }
+            return 0;
+}
+
