a912ba302e2345b251ad78955c29693b5330ad7f
ramya <ramyapriyasankar@gmail.com>
2024-09-27 08:28:26 +0530

    1.Cricket World Cup Qualifier

---
+++ 1.Cricket World Cup Qualifier.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+	int score;
+	scanf("%d", &score);
+	if (score>=12)
+	{
+	    printf("Yes");
+	    
+	}
+    else
+    {
+        printf("No");
+    }
+    return 0;
+}
+
