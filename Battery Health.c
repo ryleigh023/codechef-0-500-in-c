1e876169a47bb3ccbff58cd684746c0ff0b99be9
ramya <ramyapriyasankar@gmail.com>
2024-09-27 10:35:03 +0530

    Battery Health

---
+++ Battery Health.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+	int t, x;
+	scanf("%d", &t);
+	while (t--)
+	{
+	    scanf("%d", &x);
+	    if (x>=80)
+	    {
+	        printf("YES\n");
+	       
+	    }
+	    else
+	    {
+	        printf("NO\n");
+	        
+	    }
+	}
+	return 0;
+
+}
