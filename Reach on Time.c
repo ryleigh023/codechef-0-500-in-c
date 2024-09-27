502f96be8816eb854e1b01abf6ece66a8e58767a
ramya <ramyapriyasankar@gmail.com>
2024-09-27 08:43:30 +0530

    Reach on Time

---
+++ Reach on Time.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+	int t, x;
+	scanf("%d", &t);
+	while (t--)
+	{   scanf("%d", &x);
+	    if (x>=30)
+	    {
+	        printf("YES\n");
+	    }
+	    else
+	    {
+	        printf("NO\n");
+	    }
+	}   
+	return 0;
+
+}
