25f43a0052e74086e90c1f3726434f300a7f4559
ramya <ramyapriyasankar@gmail.com>
2024-09-28 10:54:18 +0530

    Sleep deprivation

---
+++ Sleep deprivation.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+	int t, x;
+	scanf("%d", &t);
+	while (t--)
+	{
+	    scanf("%d", &x);
+	    if (x< 7)
+	    {
+	        printf("Yes\n");
+	    }
+	    else 
+	    {
+	        printf("No\n");
+	        
+	    }
+	}
+    return 0;
+}
