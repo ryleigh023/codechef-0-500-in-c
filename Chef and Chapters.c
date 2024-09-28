cad0039e01f9d4afdbc933d067432f337cde8fc1
ramya <ramyapriyasankar@gmail.com>
2024-09-28 11:11:14 +0530

    Create Chef and Chapters.c

---
+++ Chef and Chapters.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+	int t, x, y, z;
+	scanf("%d", &t);
+	while (t--)
+	{
+	    scanf("%d %d %d", &x,  &y, &z);
+	    printf("%d\n", x*y*z);
+	    
+	}
+	return 0;
+	
+
+}
+
