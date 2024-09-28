033797019aa5ba331dde4046a18fb002e103146d
ramya <ramyapriyasankar@gmail.com>
2024-09-28 09:31:45 +0530

    Determine the Score

---
+++ Determine the Score.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+	int t, x, n, a;
+	scanf("%d", &t);
+	while (t--)
+	{
+	    scanf("%d %d", &x, &n);
+	    a = x/10 * n;
+	    printf("%d\n", a);
+	    
+	
+	}
+	return 0;
+
+}
+
