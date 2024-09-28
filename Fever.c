2040a25a3abb4a1564a8aa02f86c550e5b3e47ca
ramya <ramyapriyasankar@gmail.com>
2024-09-28 10:52:10 +0530

    Fever

---
+++ Fever.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+	int t, x;
+	scanf("%d", &t);
+	while (t--)
+	{
+	    scanf("%d", &x);
+	    if (x>98)
+	    {
+	        printf("Yes\n");
+	    }
+	    else 
+	    {
+	        printf("No\n");
+	    }
+	    
+	}
+	return 0;
+
+}
+
