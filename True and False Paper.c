989c2b967073cf82514fb492a3df364f60b82a13
ramya <ramyapriyasankar@gmail.com>
2024-09-29 16:11:18 +0530

    Create True and False Paper.c

---
+++ True and False Paper.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+	int t, n, k, score;
+	scanf("%d", &t);
+	while (t--)
+	{
+	    scanf("%d %d", &n, &k);
+	    score = n-k;
+	    printf("%d\n", score );
+	    
+	}
+	return 0;
+
+}
+
