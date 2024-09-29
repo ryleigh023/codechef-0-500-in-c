a369ce505602893c1fbd6584d4acc74ff6f973d0
ramya <ramyapriyasankar@gmail.com>
2024-09-29 19:35:21 +0530

    Create Is it hot or cold.c

---
+++ Is it hot or cold.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int t, c;
+	scanf("%d", &t);
+	while (t--)
+	{
+	    scanf("%d", &c);
+	    if (c>20) printf("hot\n");
+	    else printf("cold\n");
+	    
+	}
+	return 0;
+
+}
+
