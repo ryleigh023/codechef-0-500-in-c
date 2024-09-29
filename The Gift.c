9a0e95348e824cd72f4e920522d067af4150cc52
ramya <ramyapriyasankar@gmail.com>
2024-09-29 15:58:36 +0530

    Create The Gift.c

---
+++ The Gift.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+	int x, n, m;
+	scanf("%d %d %d", &x, &n, &m);
+	if (x+m>=n)
+	{
+	    printf("yes\n");
+	}
+	else
+	{
+	    printf("no\n");
+	}
+	return 0;
+
+}
+
